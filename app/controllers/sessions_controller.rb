c3393af8bd964db8e7f7e9740f1df8f36e8955b7
IsabelDePapel <isuchanek@gmail.com>
2017-10-16 13:09:47 -0700

    route log in to github

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -31,4 +31,28 @@ class SessionsController < ApplicationController
     flash[:result_text] = "Successfully logged out"
     redirect_to root_path
   end
+
+  def create
+    auth_hash = request.env['omniauth.auth']
+    ap auth_hash
+
+    @user = User.find_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
+
+    if @user
+      session[:user_id] = @user.id
+      flash[:success] = "#{@user.username} is logged in"
+    else
+      @user = User.new uid: auth_hash['uid'], provider: auth_hash['provider'], username: auth_hash['info']['nickname'], email: auth_hash['info']['email']
+
+      # only reason this wouldn't work is if omni auth didn't give a provider or db was down
+      if @user.save
+        session[:user_id] = @user.id
+        flash[:success] = "Welcome #{@user.username}"
+      else
+        flash[:error] = "Unable to save user!"
+      end
+    end
+
+    redirect_to root_path
+  end
 end
