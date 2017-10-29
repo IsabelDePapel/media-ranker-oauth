b31e92e5d6babd63545c0f1810ce9b0283d0f5d7
IsabelDePapel <isuchanek@gmail.com>
2017-10-28 18:44:42 -0700

    fix model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,12 +28,12 @@ describe User do
 
     it "requires a unique username" do
       username = "test username"
-      user1 = User.new(username: username)
+      user1 = User.new(username: username, email: "foo@bar.com", uid: 1, provider: :github)
 
       # This must go through, so we use create!
       user1.save!
 
-      user2 = User.new(username: username)
+      user2 = User.new(username: username, email: "bar@foo.com", uid: 2, provider: :github)
       result = user2.save
       result.must_equal false
       user2.errors.messages.must_include :username
